7317d046ce9b2cae93c418eae42623de10f5d4e3
Israel AyoOluwa Adeleke <izzyfresher@gmail.com>
2022-09-02 15:56:13 +0100

    Created Boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
